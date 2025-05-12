a772df96f7e384fedf268a6f1119fb372d651ede
Arcade69 <65565653+Arcade69@users.noreply.github.com>
2025-05-12 14:49:18 +0200

    Create Vecteur.java

---
+++ Projet_bruitage/src/Vecteur.java
@@ -0,0 +1,11 @@
+public class Vecteur {
+    public double[] vecteurs;
+    public int x;
+    public int y;
+
+    public Vecteur(double[] vecteur, int x, int y) {
+        this.vecteurs = vecteur;
+        this.x = x;
+        this.y = y;
+    }
+}

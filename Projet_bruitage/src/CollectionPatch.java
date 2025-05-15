fd53113985ac95905adede4a67a949cc39446266
manonphani <manon.phtg@gmail.com>
2025-05-15 11:06:13 +0200

    Update CollectionPatch.java

--- Projet_bruitage/src/CollectionPatch.java
+++ Projet_bruitage/src/CollectionPatch.java
@@ -2,35 +2,41 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
+/**
+ *La classe CollectionPatch représente une collection de Patch 
+ *qui nous sert à gérer l'ajout, la suppression et la vectorisation des patchs.
+*/
 
 public class CollectionPatch {
+    //Liste de Patchs stockés
     private List<Patch> patchs;
+    //Nombre de Patchs
     private int nbPatch;
-
+    //Initialisation de constructeur
     public CollectionPatch() {
         this.patchs = new ArrayList<>();
         this.nbPatch = 0;
     }
-
+    //Ajout de Patchs
     public void addPatch(Patch patch) {
         patchs.add(patch);
         nbPatch++;
     }
-
+    //Suppression de Patchs
     public void removePatch(Patch patch) {
         if (patchs.remove(patch)) {
             nbPatch--;
         }
     }
-
+    //Renvoyer le nombre de Patchs
     public int getCount() {
         return nbPatch;
     }
-
+    //Renvoyer la liste de Patchs
     public List<Patch> getPatches() {
         return patchs;
     }
-    
+    //Convertir les Patchs en liste de vecteurs 
     public static List<Vecteur> vectorPatchs(List<Patch> patchs) {
         List<Vecteur> listvecteurs = new ArrayList<>();
 

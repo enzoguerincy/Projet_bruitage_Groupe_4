44292c7225d2263444e6cbb48d8ba4f1748e9b4a
Arcade69 <65565653+Arcade69@users.noreply.github.com>
2025-05-17 14:43:05 +0200

    Add files via upload

---
+++ Projet_bruitage/src/Vecteur.java
@@ -0,0 +1,18 @@
+public class Vecteur {
+    public double[] valeurs;
+    public int x;
+    public int y;
+    
+    public Vecteur(double[] valeurs) {
+        this.valeurs = valeurs;
+    }
+
+    public Vecteur(double[] valeurs, int x, int y) {
+        this.valeurs = valeurs;
+        this.x = x;
+        this.y = y;
+    }
+    public int dimension() {
+        return valeurs.length;
+    }
+}

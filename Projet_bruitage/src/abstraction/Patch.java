895eb755be2fbec39a4927a99383124c73c89e93
nassmaghrebi <159530445+nassmaghrebi@users.noreply.github.com>
2025-05-25 23:02:38 +0200

    Update Patch.java

--- Projet_bruitage/src/abstraction/Patch.java
+++ Projet_bruitage/src/abstraction/Patch.java
@@ -2,10 +2,28 @@ package abstraction;
 import java.awt.image.BufferedImage;
 
 public class Patch {
+	/**
+     * L'image correspondant au patch (sous-image de l'image originale).
+     */
     public BufferedImage image;
+
+    /**
+     * Coordonnée horizontale (colonne) du coin supérieur gauche du patch dans l'image originale.
+     */
     public int x;
+
+    /**
+     * Coordonnée verticale (ligne) du coin supérieur gauche du patch dans l'image originale.
+     */
     public int y;
 
+    /**
+     * Construit un nouveau patch avec son image et ses coordonnées dans l'image originale.
+     *
+     * @param image L'image correspondant au patch.
+     * @param x     Position horizontale du patch dans l'image originale.
+     * @param y     Position verticale du patch dans l'image originale.
+     */
     public Patch(BufferedImage image, int x, int y) {
         this.image = image;
         this.x = x;
